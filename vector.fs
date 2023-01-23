0c3c138713d00076f5e42c3a8ef92c79d3d72bfc
LucasAub <lucas.aubard@algosup.com>
2023-01-23 16:28:52 +0100

    Upgrade dll files

--- vector.fs
+++ vector.fs
@@ -1,19 +1,12 @@
 // fichier F#
 open System.Runtime.InteropServices
 
-type Vector2 = {
-    x : double
-    y : double
-}
 
-type Vector3 = {
-    x : double
-    y : double
-    z : double
-}
+
+
 
 [<DllImport("example.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern Vector2 Vector2_new(double x, double y)
+extern Vector2.Vector2 Vector2(double x, double y)
 
 [<DllImport("example.dll", CallingConvention = CallingConvention.Cdecl)>]
 extern double Vector2_distanceTo(Vector2, Vector2)
@@ -42,16 +35,16 @@ extern Vector3 Vector3_midpoint(Vector3, Vector3)
 [<DllImport("example.dll", CallingConvention = CallingConvention.Cdecl)>]
 extern double Vector3_percentDistance(Vector3, Vector3, double)
 
-let v2 = Vector2_new(1.0, 2.0)
-let v2_2 = Vector2_new(2.0, 3.0)
-let dist = Vector2_distanceTo(v2, v2_2)
-Vector2_vectorMovement(v2, 2.0, 3.0)
-let mid = Vector2_midpoint(v2, v2_2)
-let percent = Vector2_percentDistance(v2, v2_2, 50.0)
+Vector2(1.0, 2.0)
+// let v2_2 = Vector2_new(2.0, 3.0)
+// let dist = Vector2_distanceTo(v2, v2_2)
+// Vector2_vectorMovement(v2, 2.0, 3.0)
+// let mid = Vector2_midpoint(v2, v2_2)
+// let percent = Vector2_percentDistance(v2, v2_2, 50.0)
 
-let v3 = Vector3_new(1.0, 2.0, 3.0)
-let v3_2 = Vector3_new(2.0, 3.0, 4.0)
-let dist3 = Vector3_distanceTo(v3, v3_2)
-Vector3_vectorMovement(v3, 2.0, 3.0, 4.0)
-let mid3 = Vector3_midpoint(v3, v3_2)
-let percent3 = Vector3_percentDistance(v3, v3_2, 50.0)
+// let v3 = Vector3_new(1.0, 2.0, 3.0)
+// let v3_2 = Vector3_new(2.0, 3.0, 4.0)
+// let dist3 = Vector3_distanceTo(v3, v3_2)
+// Vector3_vectorMovement(v3, 2.0, 3.0, 4.0)
+// let mid3 = Vector3_midpoint(v3, v3_2)
+// let percent3 = Vector3_percentDistance(v3, v3_2, 50.0)
