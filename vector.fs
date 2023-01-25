0bdf02934685b022028bce0c4bf01109dc1aa73f
Thomas Planchard <91249646+thomas-planchard@users.noreply.github.com>
2023-01-25 15:08:31 +0100

    test

--- vector.fs
+++ vector.fs
@@ -1,8 +1,15 @@
 
 open System.Runtime.InteropServices
 
-[<DllImport("Project_2.dll", CallingConvention= CallingConvention.Cdecl)>]
+[<DllImport("mydll.dll", CallingConvention= CallingConvention.Cdecl)>]
 extern int calculateSum(int a, int b)
 
 let result = calculateSum(3, 4)
 printfn "The sum of 3 and 4 is %d" result
+
+// [<DllImport("mydll.dll", CallingConvention= CallingConvention.Cdecl)>]
+// extern double Vector2(double x, double y)
+
+
+
+// let result = Vector2(3.0, 4.0)
