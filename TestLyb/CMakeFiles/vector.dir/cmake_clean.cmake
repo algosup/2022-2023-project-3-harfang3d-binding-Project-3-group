bcc2894632ae2415c2bcd7b673104b73477e6192
Thomas Planchard <91249646+thomas-planchard@users.noreply.github.com>
2023-01-31 14:02:47 +0100

    test

---
+++ TestLyb/CMakeFiles/vector.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vector.dir/Users/thomasplanchard/Documents/2022-2023-project-3-harfang3d-binding-Project-3-group/vector_test.cpp.o"
+  "CMakeFiles/vector.dir/Users/thomasplanchard/Documents/2022-2023-project-3-harfang3d-binding-Project-3-group/vector_test.cpp.o.d"
+  "lib/libvector.dylib"
+  "lib/libvector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- vector.fs
+++ vector.fs
@@ -1,20 +1,45 @@
 
 open System.Runtime.InteropServices
 
-// [<DllImport("mydll.dll", CallingConvention= CallingConvention.Cdecl)>]
-// extern int calculateSum(int a, int b)
+// ! Structure 
+[<Struct;StructLayout(LayoutKind.Sequential)>]
+type Vector2 =
+    val mutable x: double
+    val mutable y: double
+    new(x: double, y: double) = {x = x; y = y}
 
-// let result = calculateSum(3, 4)
-// printfn "The sum of 3 and 4 is %d" result
+[<Struct;StructLayout(LayoutKind.Sequential)>]
+type Vector3 =
+    val mutable x: double
+    val mutable y: double
+    val mutable z: double
+    new(x: double, y: double, z: double) = {x = x; y = y; z = z}
 
-[<DllImport("mydll.dll", CallingConvention= CallingConvention.Cdecl)>]
-extern double Vector2(double x, double y)
+// ! DLLImport Function
+[<DllImport("TestLyb/lib/libvector.dylib")>]
+extern double distanceTo(Vector2 pos)
 
-let result = Vector2(3.0, 4.0)
-printfn "The length of the vector (3, 4) is %f" result
+// [<DllImport("TestLyb/lib/libvector.dylib")>]
+// extern double percentDistance(Vector2 pos, double percentOfDistance)
 
+// [<DllImport("TestLyb/lib/libvector.dylib")>]
+// extern double distanceTo(Vector3 pos)
 
-[<DLKz>("libVectors.dylib", CallingConvention= CallingConvention.Cdecl)>]
-extern double Vector2(double x, double y)
+// [<DllImport("TestLyb/lib/libvector.dylib")>]
+// extern double percentDistance(Vector3 pos, double percentOfDistance)
 
-// let result = Vector2(3.0, 4.0)
+// ! Vector 2 
+let v2 = Vector2(1.0, 2.0)
+let v2DistanceTo = Vector2_distanceTo(v2)
+// let v2PercentDistance = percentDistance(v2, 0.5)
+
+// ! Vector 3
+// let v3 = Vector3(1.0, 2.0, 3.0)
+// let v3DistanceTo = distanceTo(v3)
+// let v3PercentDistance = percentDistance(v3, 0.5)
+
+// ! Print 
+printfn "Distance to origin in Vector 2: %f" v2DistanceTo
+// printfn "Percent distance to origin in Vector 2: %f" v2PercentDistance
+// printfn "Distance to origin in Vector 3: %f" v3DistanceTo
+// printfn "Percent distance to origin in Vector 3: %f" v3PercentDistance
